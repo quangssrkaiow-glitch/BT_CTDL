ea7294d642b4fac6b3bb34372887470949abf8f4
quangssrkaiow-glitch <quangssrkaiow@gmail.com>
2026-06-10 22:41:49 +0700

    Thêm nút và quay để cây cân bằng

--- BTTuan14/CayCanBang.cpp
+++ BTTuan14/CayCanBang.cpp
@@ -23,3 +23,84 @@ Node* taoNode(int x) {
     p->height = 1;
     return p;
 }
+// tìm số lớn hơn
+int max(int a, int b) {
+    if (a > b)
+        return a;
+    return b;
+}
+
+// quay phải
+Node* quayPhai(Node* y) {
+    Node* x = y->left;
+    Node* T = x->right;
+
+    x->right = y;
+    y->left = T;
+
+    y->height = max(height(y->left), height(y->right)) + 1;
+    x->height = max(height(x->left), height(x->right)) + 1;
+    return x;
+}
+// quay trái
+Node* quayTrai(Node* x) {
+    Node* y = x->right;
+    Node* T = y->left;
+
+    y->left = x;
+    x->right = T;
+
+    x->height = max(height(x->left), height(x->right)) + 1;
+    y->height = max(height(y->left), height(y->right)) + 1;
+
+    return y;
+}
+// tính hệ số cân bằng
+int getBalance(Node* p) {
+    if (p == NULL)
+        return 0;
+
+    return height(p->left) - height(p->right);
+}
+
+// thêm vào cây AVL
+Node* themNode(Node* root, int x) {
+
+    if (root == NULL)
+        return taoNode(x);
+
+    if (x < root->data)
+        root->left = themNode(root->left, x);
+
+    else if (x > root->data)
+        root->right = themNode(root->right, x);
+
+    else
+        return root;
+
+    root->height =
+        max(height(root->left), height(root->right)) + 1;
+
+    int balance = getBalance(root);
+    // Left Left
+    if (balance > 1 && x < root->left->data)
+        return quayPhai(root);
+
+    // Right right
+    if (balance < -1 && x > root->right->data)
+        return quayTrai(root);
+
+    // Left right
+    if (balance > 1 && x > root->left->data) {
+        root->left = quayTrai(root->left);
+        return quayPhai(root);
+    }
+
+    // Right Left
+    if (balance < -1 && x < root->right->data) {
+        root->right = quayPhai(root->right);
+        return quayTrai(root);
+    }
+
+    return root;
+}
