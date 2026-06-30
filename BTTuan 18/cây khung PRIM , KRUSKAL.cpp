24025a9998f2ce52afdd490d0e13a8d164bb3b1b
quangssrkaiow-glitch <quangssrkaiow@gmail.com>
2026-06-30 12:48:41 +0700

    thuat toan kruskal

--- BTTuan 18/cây khung PRIM , KRUSKAL.cpp
+++ BTTuan 18/cây khung PRIM , KRUSKAL.cpp
@@ -15,3 +15,25 @@ Canh canh[] = {
     {8,7,11},{7,6,12},{5,6,13}
 };
 const int E = 13;
+int cha[V];
+int find(int x) { return cha[x] == x ? x : cha[x] = find(cha[x]); }
+
+void kruskal(){
+    Canh e[E];
+    copy(canh, canh + E, e);
+    sort(e, e + E, [](Canh a, Canh b) { return a.w < b.w; });
+
+    for (int i = 0; i < V; i++) cha[i] = i;
+
+    cout << "KRUSKAL " << endl;
+    int tongTrongSo = 0;
+    for (int i = 0; i < E; i++) {
+        int u = e[i].u, v = e[i].v, w = e[i].w;
+        if (find(u) != find(v)) {
+            cha[find(u)] = find(v);
+            tongTrongSo += w;
+            cout << ten[u] << " - " << ten[v] << endl;
+        }
+    }
+    cout << "Tong trong so MST: " << tongTrongSo << endl;
+}
